73f74ca1ff05b3806ea48508a6a5548fda12905f
sergiuszorgo <67199252+sergiuszorgo@users.noreply.github.com>
2020-10-02 20:16:29 +0300

    Модуль 2. Занятие 3. Массивы

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
 "use strict";
-const staff = ["Mango", "Poly", "Ajax", "Kiwi"];
-const query = "Ajax";
-// let result = "Сотрудника нет";
-
-// for (const employee of staff) {
-//   console.log(employee);
-//   if (query === employee) {
-//     result = "Сотрудник на работе";
-//     break;
-//   }
-// }
+// const staff = ["Mango", "Poly", "Ajax", "Kiwi"];
+// const query = "Ajax";
+// const result = staff.includes(query) ? "На работе" : "Нет такого";
 // console.log(result);
-const result = staff.includes(query) ? "На работе" : "Нет такого";
-console.log(result);
+// ---
+// const oldStaff = ["Mango", "Poly", "Ajax"];
+// const newStaff = ["Chelsy", "Kiwi"];
+
+// const staff = oldStaff.concat(newStaff);
+// console.log("oldStaff", oldStaff);
+// console.log("newStaff", newStaff);
+// console.log(staff);
+
+// console.log([...oldStaff, ...newStaff]);
+// ---
+const cards = ["card-1", "card-2", "card-3", "card-4", "card-5"];
+
+console.log(cards);
+
+cards.splise(1, 2);
+
+console.log(cards);
