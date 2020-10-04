c93fb414c818ffdc4a5c9d586ebfd3886c7a7d1a
sergiuszorgo <67199252+sergiuszorgo@users.noreply.github.com>
2020-10-04 22:50:41 +0300

    Задача 2-9

--- script.js
+++ script.js
@@ -129,3 +129,157 @@
 // const r1 = findFriends(staff, "Poly");
 // console.log(r1);
 // ---
+// УДАЛЕНИЕ КАРТЫ
+// const cards = ["card-1", "card-2", "card-3", "card-4", "card-5"];
+// console.log(cards);
+
+// const removeCard = function (allCards, cardToRemove) {
+//   const index = allCards.indexOf(cardToRemove);
+//   allCards.splice(index, 1);
+// };
+// removeCard(cards, "card-3");
+// console.log(cards);
+// ---
+// ДОБАВЛЯЕМ КАРТУ
+// const cards = ["card-1", "card-2", "card-3", "card-4", "card-5"];
+// console.log(cards);
+
+// const addCard = function (allCards, cardToInsert, index) {
+//   allCards.splice(index, 0, cardToInsert);
+// };
+
+// addCard(cards, "card-100", 2);
+// console.log(cards);
+// ---
+// 2 - 2  ПОДСЧЁТ СТОИМОСТИ ГРАВИРОВКИ СЛОВ
+// const calculateEngravingPrice = (message = "", pricePerWord = 0) =>
+//   message.split(" ").length * pricePerWord;
+
+// console.log(
+//   calculateEngravingPrice(
+//     "Proin sociis natoque et magnis parturient montes mus",
+//     10
+//   )
+// );
+// ---
+// 2 - 3  ПОИСК САМОГО ДЛИННОГО СЛОВА
+// function findLongestWord(string = "") {
+//   let words = string.split(" ");
+//   let longWord = words[0];
+//   for (let i = 0; i < words.length; i += 1) {
+//     if (longWord.length < words[i].length) {
+//       longWord = words[i];
+//     }
+//   }
+//   return longWord;
+// }
+
+// console.log(findLongestWord("The quick brown fox jumped over the lazy dog"));
+// ---
+// 2 - 4  Форматирование строки в зависимости от длинны строки
+// function formatString(string, maxLength = 40) {
+//   return string.length < maxLength
+//     ? string
+//     : string.slice(0, maxLength) + "...";
+// }
+
+// console.log(formatString("Vestibulum facilisis, purus nec pulvinar iaculis."));
+// ---
+// 2 - 5  ПРОВЕРКА НА НАЛИЧИЕ СЛОВ
+// function checkForSpam(str) {
+//   "use strict";
+//   return (
+//     str.toLowerCase().includes(`spam`) || str.toLowerCase().includes(`sale`)
+//   );
+// }
+
+// console.log(checkForSpam("Get best sale offers now!")); //true
+// console.log(checkForSpam("Latest technology news")); // false
+// ---
+// ? ? ? ? ? ? ? ? ? ? ? ? ?? ? ? ? ? ?? ? ? ? ?? ? ?
+// const getItemString = function (array) {
+//   "use strict";
+//   let result = "";
+//   for (let i = 0; i < array.length; i += 1) {
+//     result += `${i + 1} - ${array[i]}\n`;
+//   }
+//   return result;
+// };
+
+// console.log(getItemString(["Mango", "Poly", "Ajax", "Kiwi"]));
+// ---
+// Задача 2-6
+// function mapArray(array) {
+//   "use strict";
+//   const numbers = new Array(array.length);
+//   for (let i = 0; i < array.length; i += 1) {
+//     numbers[i] = array[i] * 10;
+//   }
+//   return numbers;
+// }
+
+// console.log(mapArray([-2, 0, 2])); // [-20, 0, 20]
+// console.log(mapArray([-2.5, 0, 2.5])); // [-25, 0, 25]
+// ---
+// Задача 2-7
+// function filterArray(array) {
+//   "use strict";
+//   const numbers = [];
+//   for (let i = 0; i < array.length; i += 1) {
+//     // Write code under this line
+//     if (Number.isFinite(array[i])) {
+//       numbers.push(array[i]);
+//     }
+//   }
+//   return numbers;
+// }
+
+// console.log(filterArray([-2, 0, 2])); // [-2, 0, 2]
+// ---
+// Задача 2-8
+// function reduceArray(array) {
+//   "use strict";
+//   let total = 0;
+//   // Write code under this line
+//   for (let i = 0; i < array.length; i += 1) {
+//     total += array[i];
+//   }
+//   return total;
+// }
+
+// console.log(reduceArray([1, 2, 3])); // 6
+// ---
+// Задача 2-9
+function isLoginValid(login, min = 4, max = 16) {
+  // Write code under this line
+  let LongUserName = login.length >= min && login.length <= max;
+  return LongUserName;
+}
+
+function isLoginUnique(allLogins, login) {
+  "use strict";
+  // Write code under this line
+  return allLogins.includes(login);
+}
+
+function addLogin(allLogins, login) {
+  "use strict";
+  const SUCCESS = "Логин успешно добавлен!";
+  const REFUSAL = "Такой логин уже используется!";
+  const ERROR = "Ошибка! Логин должен быть размером от 4 до 16 символов";
+  let message;
+  // Write code under this line
+  if (isLoginValid(login) === false) {
+    return (message = ERROR);
+  } else if (isLoginUnique(allLogins, login) === false) {
+    return (message = REFUSAL);
+  } else {
+    allLogins.push(login);
+    return (message = SUCCESS);
+  }
+}
+
+const logins = ["Mango", "robotGoogles", "Poly", "Aj4x1sBozz", "qwerty123"];
+
+console.log(addLogin(logins, "Ajax"));
+// 'Логин успешно добавлен!'
